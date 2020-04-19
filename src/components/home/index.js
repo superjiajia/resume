6c5a2386c15b923f83d9ccf9e47526f665293738
Wang Jia <wangjia125513819@foxmail.com>
2020-04-19 18:30:23 +0800

    init

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './App.css';
+import './index.styl';
 
-function App() {
+function Index() {
   return (
     <div className="App">
 
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+export default Index;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Index from './components/home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
