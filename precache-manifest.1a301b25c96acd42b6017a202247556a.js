74e7d8f7719ff12753acf269ed3c50f9d5517483
Wang Jia <wangjia125513819@foxmail.com>
2020-05-24 19:08:25 +0800

    Updates

---
+++ precache-manifest.1a301b25c96acd42b6017a202247556a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ed23211450e3f32511ac9c977d8d57",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4016f74f9f626bef7ace",
+    "url": "./static/css/2.95e34c3a.chunk.css"
+  },
+  {
+    "revision": "7c4e2bba81ed91cbd6be",
+    "url": "./static/css/main.7a9d7deb.chunk.css"
+  },
+  {
+    "revision": "4016f74f9f626bef7ace",
+    "url": "./static/js/2.eb83a757.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.eb83a757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4e2bba81ed91cbd6be",
+    "url": "./static/js/main.b00778f0.chunk.js"
+  },
+  {
+    "revision": "d0f6aadfe947fa4aff19",
+    "url": "./static/js/runtime-main.6349f449.js"
+  },
+  {
+    "revision": "171d5f82024c50e6e9691a65a1f60cc5",
+    "url": "./static/media/dizhi.171d5f82.png"
+  },
+  {
+    "revision": "42fa40aa432c72e3ba18c5f6096eeacb",
+    "url": "./static/media/shuye.42fa40aa.png"
+  },
+  {
+    "revision": "f648c4af70e3999a7c8fd398d33c54e1",
+    "url": "./static/media/wechat.f648c4af.jpg"
+  }
+]);
